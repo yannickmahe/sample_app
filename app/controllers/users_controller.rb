be6c9a25478320385d910b18d6ac8767bc211db5
Yannick <yannick@yannick-G73Sw.(none)>
2012-10-23 22:39:48 +0200

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  protect_from_forgery
+  include SessionsHelper
+
   def new
   	@user = User.new
   end
@@ -10,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
